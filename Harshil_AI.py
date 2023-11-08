1d06960350d07a2cc34a424c3886ab9deb932c4c
D Harshil Kumar Reddy <114159242+Harshil-Kumar@users.noreply.github.com>
2023-11-08 22:31:00 +0530

    Add files via upload

--- Harshil_AI.py
+++ Harshil_AI.py
@@ -1,32 +1,32 @@
 n=int(input("Enter the number of 4-letter prompts: "))
-a=[]
+word=[]
 
 for i in range(n):
     prompt = input(f"Enter prompt {i + 1}: ")
     if len(prompt) != 4:
         print("Prompt must be 4 letters long. Please try again.")
         exit(1)
-    a.append(prompt)
+    word.append(prompt)
 
-b=[]
-c=[]
+x=[]
+y=[]
 
-for i in range(len(a) - 1):
-    for j in range(len(a[0])):
-        if a[i][j:]==a[i+1][:-j]:
-            c.append(a[i])
-            c.append(a[i+1])
-            b.append(a[i][j:])
+for i in range(len(word) - 1):
+    for j in range(len(word[0])):
+        if word[i][j:]==word[i+1][:-j]:
+            y.append(word[i])
+            y.append(word[i+1])
+            x.append(word[i][j:])
 
-d=''.join([ele for ele in b])
-n=len(b[0])
-d=c[0][:-n]+d
-final=d+c[len(c)-1][n:]
+d=''.join([ele for ele in x])
+n=len(x[0])
+d=y[0][:-n]+d
+final=d+y[len(y)-1][n:]
 
 print("\n")
-print("Common Elements : ", b)
+print("Common Elements : ", x)
 print("Final Word : ", final)
 
-for i in a:
+for i in word:
     if i not in final:
         print("Other Word : ", i)
